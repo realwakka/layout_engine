3573fb358685f25bf2608e053bcad7ac5b2e9d1b
realwakka <realwakka@gmail.com>
2016-12-13 00:45:41 +0900

    add view

---
+++ src/view/view.cc
@@ -0,0 +1,23 @@
+#include "view.h"
+
+namespace le {
+
+View::View()
+    : width_(0),
+      height_(0),
+      x_(0),
+      y_(0)
+{}
+
+View::~View()
+{}
+
+void View::Layout()
+{}
+
+void View::Paint()
+{}
+
+
+
+}  // le
---
+++ src/view/view.h
@@ -0,0 +1,33 @@
+namespace le {
+
+class View
+{
+ public:
+  View();
+  virtual ~View();
+
+  void Layout();
+  void Paint();
+
+  int GetX() const { return x_; }
+  int GetY() const { return y_; }
+
+  void SetX(int x) { x_ = x; }
+  void SetY(int y) { y_ = y; }
+
+  int GetWidth() const { return width_; }
+  int GetHeight() const { return height_; }
+
+  void SetWidth(int width) { width_ = width; }
+  void SetHeight(int height) { height_ = height; }
+
+ private:
+  int width_;  
+  int height_;
+  int x_;
+  int y_;
+
+};
+
+
+}  // le
