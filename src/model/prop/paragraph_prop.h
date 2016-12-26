4bf45bcd2a78a9de31d7bf7b34754fd6bdf717cd
sidong_yang <sidong_yang@tmax.co.kr>
2016-12-26 22:32:54 +0900

    add indentation

--- src/model/prop/paragraph_prop.h
+++ src/model/prop/paragraph_prop.h
@@ -3,11 +3,42 @@
 
 namespace le {
 
+class Indentation
+{
+ public:
+  Indentation();
+  virtual ~Indentation();
+
+  int GetStart() const { return start_; }
+  int GetEnd() const { return end_; }
+  int GetFirst() const { return first_; }
+  int GetHanging() const { return hanging_; }
+
+  int SetStart(int start) { start_ = start; }
+  int SetEnd(int end) { end_ = end; }
+  int SetFirst(int first) { first_ = first; }
+  int SetHanging(int hanging) { hanging_ = hanging; }
+  
+ private:
+  int start_;
+  int end_;
+  int first_;
+  int hanging_;
+};
+
+
 class ParagraphProp
 {
  public:
   ParagraphProp();
   virtual ~ParagraphProp();
+
+  Indentation& GetIndentation() { return indentation_; }
+
+ private:
+  Indentation indentation_;
+
+  
 
   
 };
