48cf96c30a1f0a5df5f5ee898e9be8827815d9f3
realwakka <realwakka@gmail.com>
2017-01-18 17:50:22 +0900

    window size impl

--- lab/web_office/app.js
+++ lab/web_office/app.js
@@ -8,33 +8,42 @@ app.get('/', function(req, res){
     res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+var client = net.createConnection("/tmp/sock1", function() {
+    console.log('connected to c++ server!');
 
-    var client = net.createConnection("/tmp/sock1", function() {
-	console.log('connected to server!');
-	var data  = 'world!';
-	console.log('serve send data : '+data);
-    });
-
-    
-    socket.on('disconnect', function(){
-	console.log('user disconnected');
-    });
-
-    socket.on('chat message', function(ch){
-	console.log(ch);
-	client.write(ch);
-    });
-
-    client.on('data',function(data){
-	fs.readFile('/home/sidong/layout_engine/build/output.bmp', function(err, buf){
-	    socket.emit('image', { image: true, buffer: buf.toString('base64') });
-	    console.log('image file is initialized');
+    io.on('connection', function(socket){
+	console.log('a user connected');
+	console.log('give me the window size!');
+	socket.emit('window size request');
+	
+	socket.on('disconnect', function(){
+	    console.log('user disconnected');
 	});
+
+	socket.on('key event', function(ch){
+	    console.log('key event :' + ch);
+	    client.write(ch);
+	});
+
+	socket.on('window size', function(size){
+	    console.log(size);
+	    client.write(size);
+	});
+
+
+	client.on('data',function(data){
+	    fs.readFile('/home/sidong/layout_engine/build/output.bmp', function(err, buf){
+		socket.emit('image', { image: true, buffer: buf.toString('base64') });
+		console.log('image file is initialized');
+	    });
+	});
+
     });
+    
 
 });
+
+
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
