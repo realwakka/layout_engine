cd4e88392855c7a5414aae83d06851e49d4603d3
realwakka <realwakka@gmail.com>
2017-02-08 10:01:56 +0900

    fix bugs for space scroll

--- src/web/server.cc
+++ src/web/server.cc
@@ -116,23 +116,17 @@ bool ProcessEvent(const std::string& json, le::RenderText& rendertext)
   
 }
 
-
-int callback_http(lws* wsi,
-                  lws_callback_reasons reason,
-                  void *user,
-                  void *in, size_t len)
+int CallBackLe(lws* wsi,
+               lws_callback_reasons reason,
+               void *user,
+               void *in, size_t len)
 {
+
+
   switch (reason) {
     case LWS_CALLBACK_CLIENT_WRITEABLE:
       printf("connection client established\n");
       break;
-    case LWS_CALLBACK_HTTP: {
-      char *requested_uri = (char *) in;
-      printf("requested URI: %s\n", requested_uri);
-
-      lws_serve_http_file(wsi, "index.html", "text/html", nullptr, 0);
-      break;
-    }
 
     case LWS_CALLBACK_ESTABLISHED: {
       auto rendertext_map = static_cast<std::unordered_map<lws*, RenderText*>*>(lws_get_protocol(wsi)->user);
@@ -156,9 +150,9 @@ int callback_http(lws* wsi,
       std::cout << str << std::endl;
       auto it = rendertext_map->find(wsi);
       if( it != rendertext_map->end() ) {
-        auto& rendertext = *it->second;
+        auto&& rendertext = *it->second;
         ProcessEvent(str, rendertext);
-        auto& document_view = rendertext.GetDocument().GetView();
+        auto&& document_view = rendertext.GetDocument().GetView();
         auto bitmap = new le::Bitmap(document_view.GetWidth(), document_view.GetHeight(), 3);
 
         std::cout << "painting...";
@@ -203,38 +197,50 @@ int callback_http(lws* wsi,
       std::cout << "change mode pool\n" << std::endl;
       break;
     case LWS_CALLBACK_LOCK_POLL:
-      printf("lock poll %d\n", reason);
+      std::cout << "lock poll "<< std::endl;
       break;
     case LWS_CALLBACK_UNLOCK_POLL:
-      printf("unlock poll %d\n", reason);
+      std::cout << "unlock poll "<< std::endl;
       break;
     case LWS_CALLBACK_ADD_POLL_FD:
-      printf("add pool fd %d\n", reason);
+      std::cout << "add pool fd "<< std::endl;
       break;
     case LWS_CALLBACK_DEL_POLL_FD:
-      printf("delete pool fd %d\n", reason);
+      std::cout << "delete pool fd "<< std::endl;
       break;
       
     default:
-      printf("unhandled callback %d\n", reason);
+      std::cout << "unhandled callback " << reason << std::endl;
       break;
+  }
+
+  return 0;
+  
+}
+
+
+
+int CallBackHttp(lws* wsi,
+                 lws_callback_reasons reason,
+                 void *user,
+                 void *in, size_t len)
+{
+  switch (reason) {
+    case LWS_CALLBACK_CLIENT_WRITEABLE:
+      printf("connection client established\n");
+      break;
+    case LWS_CALLBACK_HTTP: {
+      char *requested_uri = (char *) in;
+      printf("requested URI: %s\n", requested_uri);
+
+      lws_serve_http_file(wsi, "../../src/web/index.html", "text/html", nullptr, 0);
+      break;
+    }
   }
 
   return 0;
 }
 
-lws_protocols protocols[] = {
-  /* first protocol must always be HTTP handler */
-  {
-    "le_web_protocol",
-    callback_http,
-    0,
-    0,
-  },
-  {
-    NULL, NULL, 0   /* End of list */
-  }
-};
 
 
 }
@@ -256,26 +262,45 @@ void Server::Start()
   lws_context *context;
   int opts = 0;
 
-  lws_protocols protocol;
-  memset(&protocol, 0, sizeof(protocol));
-  protocol.name = "le_web_protocol";
-  protocol.callback = callback_http;
-  protocol.user = &rendertext_map_;
+
+  lws_protocols protocols[] = {
+    /* first protocol must always be HTTP handler */
+    {
+      "http_protocol",
+      CallBackHttp,
+      0,
+      0,
+    },
+    {
+      "le_web_protocol",
+      CallBackLe,
+      0,
+      0,
+      0,
+      &rendertext_map_,
+      
+    },
+    {
+      NULL, NULL, 0   /* End of list */
+    }
+  };
+
+  
+  // lws_protocols protocol;
+  // memset(&protocol, 0, sizeof(protocol));
+  // protocol.name = "le_web_protocol";
+  // protocol.callback = CallBackHttp;
+  // protocol.user = &rendertext_map_;
 
   lws_context_creation_info info;
   memset(&info, 0, sizeof(info));
   
   info.port = port;
   info.iface = interface;
-  info.protocols = &protocol;
+  info.protocols = protocols;
   info.extensions = nullptr;
-  //if (!use_ssl) {
   info.ssl_cert_filepath = NULL;
   info.ssl_private_key_filepath = NULL;
-  //} else {
-  //  info.ssl_cert_filepath = LOCAL_RESOURCE_PATH"/libwebsockets-test-server.pem";
-  //  info.ssl_private_key_filepath = LOCAL_RESOURCE_PATH"/libwebsockets-test-server.key.pem";
-  //}
   info.gid = -1;
   info.uid = -1;
   info.options = opts;
